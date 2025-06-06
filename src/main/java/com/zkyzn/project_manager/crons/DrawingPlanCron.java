81eef8063b37c1d92cc90ed71875412d19705cf0
biless <biless@qq.com>
2025-06-06 22:43:39 +0800

    feat: try to read old drawing plan

--- src/main/java/com/zkyzn/project_manager/crons/DrawingPlanCron.java
+++ src/main/java/com/zkyzn/project_manager/crons/DrawingPlanCron.java
@@ -1,7 +1,14 @@
 package com.zkyzn.project_manager.crons;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 /**
  * 定时处理图纸计划的数据初始化
@@ -10,12 +17,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class DrawingPlanCron {
 
+
+    @Value("${file.old.plan}")
+    String oldPlanDir;
+
+
     /**
      * 每隔一段时间处理图纸计划相关数据
      */
     @Scheduled(cron = "0 */5 * * * *")
-    public void ProcessDrawingPlan() {
+    public void ProcessDrawingPlan() throws IOException {
         // 读取历史图纸列表
+        try (Stream<Path> paths = Files.list(Paths.get(oldPlanDir))) {
+            paths.filter(p -> p.toString().endsWith(".xls"))
+                    .forEach(p -> {
 
+                    });
+        }
     }
 }
