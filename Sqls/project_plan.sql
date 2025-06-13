8652c081a9ecc1bd42e1a10f41464059e5a95c45
江时凯 <jiangshikai@hfhx-tech.com>
2025-06-13 10:48:50 +0800

    feat add ddl

---
+++ Sqls/project_plan.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `tab_project_plan` (
+                                    `project_plan_id` bigint NOT NULL AUTO_INCREMENT COMMENT '计划项唯一ID',
+                                    `project_id` bigint NOT NULL COMMENT '关联项目ID',
+                                    `task_package` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '任务包',
+                                    `task_description` text COLLATE utf8mb4_general_ci NOT NULL COMMENT '任务内容',
+                                    `start_date` date NOT NULL COMMENT '开始时间',
+                                    `end_date` date NOT NULL COMMENT '结束时间',
+                                    `responsible_person` varchar(50) COLLATE utf8mb4_general_ci NOT NULL COMMENT '责任人',
+                                    `department` varchar(50) COLLATE utf8mb4_general_ci NOT NULL COMMENT '科室',
+                                    `deliverable` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '成果',
+                                    `deliverable_type` varchar(50) COLLATE utf8mb4_general_ci NOT NULL COMMENT '成果类型',
+                                    `is_milestone` tinyint(1) DEFAULT '0' COMMENT '是否里程碑任务',
+                                    `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                    `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+                                    `task_status` varchar(16) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '任务状态',
+                                    `real_start_date` date DEFAULT NULL COMMENT '实际开始时间',
+                                    `real_end_date` date DEFAULT NULL COMMENT '实际结束时间',
+                                    `phase_id` bigint DEFAULT NULL COMMENT '项目阶段ID',
+                                    PRIMARY KEY (`project_plan_id`),
+                                    KEY `fk_plan_project` (`project_id`),
+                                    CONSTRAINT `fk_plan_project` FOREIGN KEY (`project_id`) REFERENCES `tab_project_info` (`project_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='项目计划表';
