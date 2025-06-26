d21af123b69127c2d153a2494af03040cf6dd658
zhangfan <zhangfan@qq.com>
2025-06-26 09:29:44 +0800

    feat: 调整资源管理器参数格式

--- src/main/java/com/zkyzn/project_manager/so/project/info/ProjectFolderReq.java
+++ src/main/java/com/zkyzn/project_manager/so/project/info/ProjectFolderReq.java
@@ -1,5 +1,7 @@
 package com.zkyzn.project_manager.so.project.info;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -9,6 +11,7 @@ import lombok.Data;
  */
 @Schema(description = "项目资源管理器请求体")
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ProjectFolderReq {
 
     @Schema(description = "文档类型（项目计划/图纸目录/生产会材料/汇报材料/二次统计/合并文档/其他）",
